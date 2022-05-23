076a52b2e6852437e027f1c65fb92c3e0f0de044
Nikita Medvedev <nikit238@gmail.com>
2022-05-23 14:52:59 +0300

    default node env set to production

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,7 +9,7 @@ import { AuthModule } from './auth/auth.module';
   imports: [
     AuthModule,
     ConfigModule.forRoot({
-      envFilePath: `.${process.env.NODE_ENV}.env`
+      envFilePath: `.${process.env.NODE_ENV || 'production'}.env`
     }),
     MongooseModule.forRoot(`mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_URL}/${process.env.DB_DATABASE}?authSource=admin`)
   ],
